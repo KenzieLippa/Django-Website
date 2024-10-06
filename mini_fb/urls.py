45ff60c0149e530f4b0c6c5594b35c5674676a80
KenzieLippa <roseycat7@gmail.com>
2024-10-06 16:52:12 -0400

    hopefully all the information and content needed, next will collect static and publish i think

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from .views import ShowAllView
 
 urlpatterns = [
-    
+    path('', ShowAllView.as_view(), name='show_all')
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Profile
+from django.views.generic import ListView
 
+class ShowAllView(ListView):
+    '''create a view to show all the portraits'''
+    model = Profile
+    template_name = 'mini_fb/show_all.html'
+    context_object_name = 'profiles'
 # Create your views here.
