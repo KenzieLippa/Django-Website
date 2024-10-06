301cf18c0044d140a310acd8278ced32c8574b49
KenzieLippa <roseycat7@gmail.com>
2024-10-06 17:02:29 -0400

    changed the profile view for gradescope

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,6 +5,6 @@ from django.views.generic import ListView
 class ShowAllView(ListView):
     '''create a view to show all the portraits'''
     model = Profile
-    template_name = 'mini_fb/show_all.html'
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
 # Create your views here.
