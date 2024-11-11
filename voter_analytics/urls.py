083aae24be143d222074b0bf770d7e8a0171f3a3
KenzieLippa <roseycat7@gmail.com>
2024-11-11 17:11:53 -0500

    finished everything i think

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path(r'', views.VoterListView.as_view(), name='voters'),
     path(r'voter', views.VoterListView.as_view(), name='voter_list'),
     path(r'voter/<int:pk>', views.VoterDetailView.as_view(), name='voter_detail'),
-
+    path(r'graphs', views.GraphView.as_view(), name="graphs"),
+    
 ]
