a032e2e1e40de473f5a154f062bed0e210c20522
KenzieLippa <roseycat7@gmail.com>
2024-12-02 19:37:25 -0500

    wrong folder name i think

--- fightingTest/views.py
+++ fightingTest/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def BaseView(req):
     # model = Profile
-    template_name = 'FightingTest/test.html'
+    template_name = 'fightingTest/test.html'
     return render(req, template_name)
 
     
